8f4697d5fa2bbf34ab298c24ce65a0a86aa6c4f1
Katie Bowers <katiebowers0210@gmail.com>
2023-07-26 13:29:23 -0400

    set up the table

--- contact list/src/App.jsx
+++ contact list/src/App.jsx
@@ -1,13 +1,10 @@
-import { useState } from 'react'
-
 import './App.css'
+import ContactList from './components/ContactList'
 
 function App() {
-  
-
   return (
     <>
-       
+       <ContactList />
     </>
   )
 }
