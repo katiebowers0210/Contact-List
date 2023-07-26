ebc0a05bf2a554f4666c9cb3b495409d4409ebd3
Katie Bowers <katiebowers0210@gmail.com>
2023-07-26 13:39:08 -0400

    fetched data and useEffect

--- contact list/src/components/ContactList.jsx
+++ contact list/src/components/ContactList.jsx
@@ -1,4 +1,5 @@
 // import React from "react"; 
+import { useEffect } from 'react';
 import { useState } from 'react'
 import ContactRow from "./ContactRow";
 
@@ -12,6 +13,22 @@ const dummyContacts = [
 export default function ContactList() { 
     const [contacts, setContacts] = useState(dummyContacts);
     // console.log('Contacts:', contacts)
+
+    useEffect(()=> {
+        async function fetchContacts() {
+            try {
+                const response = await fetch(
+                    "https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users"
+                );
+                const result = await response.json();
+                setContacts(result);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        fetchContacts()
+        console.log(contacts)
+    }, []);
 
     return ( 
           <table>
